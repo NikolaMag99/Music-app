8e2444346e729cd36e049f29283e558e632e6384
Nikola Paunovic <nikolapaunovic99@gmail.com>
2020-10-30 12:40:37 +0100

    prvi komit

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+// dobijamo info odakle smo pristupili serveru
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+// da ne moze ni jedan klijent da koristi server
+app.use(cors())
+
+/*
+app.post('/register', (req, res) => {
+    res.send({
+        message: `cao ${req.body.email}! Registrovani korisnice`
+    })
+})
+
+*/
+
+app.get('/status',(req, res) => {
+    res.send({
+        message: 'cao'
+    })
+})
+app.listen(process.env.PORT || 8083)
